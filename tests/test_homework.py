1ec9f4277c9e4e8d1b7cb3add73e9341669ac47d
primolechuga <juanestebaca123@gmail.com>
2025-10-04 08:03:21 -0500

    fix test

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -5,7 +5,7 @@ def load_data():
 
     import pandas as pd
 
-    dataset = pd.read_csv("auto_mpg.csv")
+    dataset = pd.read_csv("files/input/auto_mpg.csv")
     dataset = dataset.dropna()
     dataset["Origin"] = dataset["Origin"].map(
         {1: "USA", 2: "Europe", 3: "Japan"},
